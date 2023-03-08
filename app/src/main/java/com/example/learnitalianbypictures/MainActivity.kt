1874abaa3298238ba4859f121d33527522442a49
EvgeniiGrinko <jenek0907@mail.ru>
2023-03-08 23:52:30 +0500

    added second question page

--- app/src/main/java/com/example/learnitalianbypictures/MainActivity.kt
+++ app/src/main/java/com/example/learnitalianbypictures/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.learnitalianbypictures
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,22 +13,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        bindingClass.FirstOption.setText(R.string.il_burro)
+        bindingClass.SecondOption.setText(R.string.la_macchina)
+        bindingClass.ThirdOption.setText(R.string.la_spiaggia)
+        bindingClass.ForthOption.setText(R.string.la_testa)
+        bindingClass.GuessWord.setText(R.string.guess_answer)
+        bindingClass.NextQuestionId.setText(R.string.next)
 
         bindingClass.FirstOption.setOnClickListener{
-            bindingClass.Result.visibility = View.VISIBLE
-            bindingClass.Result.text = "Your answer isn't correct :( Try again!"
+            this.showIncorrectMessageAnswer()
         }
         bindingClass.SecondOption.setOnClickListener{
-            bindingClass.Result.visibility = View.VISIBLE
-            bindingClass.Result.text = "Your answer isn't correct :( Try again!"
+            this.showIncorrectMessageAnswer()
         }
         bindingClass.ThirdOption.setOnClickListener{
-            bindingClass.Result.visibility = View.VISIBLE
-            bindingClass.Result.text = "Your answer is correct :) Winner! It is: the beach :)"
+            this.showCorrectMessageAnswer()
         }
         bindingClass.ForthOption.setOnClickListener{
-            bindingClass.Result.visibility = View.VISIBLE
-            bindingClass.Result.text = "Your answer isn't correct :( Try again!"
+            this.showIncorrectMessageAnswer()
         }
     }
+    fun onClickGoToSecondActivity(view: View){
+        val intent = Intent(this, SecondPageActivity::class.java)
+        startActivity(intent)
+    }
+    private fun showIncorrectMessageAnswer(){
+        bindingClass.Result.setText(R.string.incorrect_answer)
+        bindingClass.Result.visibility = View.VISIBLE
+
+    }
+    private fun showCorrectMessageAnswer(){
+        bindingClass.Result.visibility = View.VISIBLE
+        bindingClass.FirstOption.visibility = View.GONE
+        bindingClass.SecondOption.visibility = View.GONE
+        bindingClass.ThirdOption.visibility = View.GONE
+        bindingClass.ForthOption.visibility = View.GONE
+        bindingClass.Result.setText(R.string.correct_answer)
+        bindingClass.NextQuestionId.visibility = View.VISIBLE
+    }
+
 }
--- app/src/main/java/com/example/learnitalianbypictures/SecondPageActivity.kt
+++ app/src/main/java/com/example/learnitalianbypictures/SecondPageActivity.kt
@@ -1,11 +1,55 @@
 package com.example.learnitalianbypictures
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.learnitalianbypictures.databinding.ActivitySecondPageBinding
 
 class SecondPageActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivitySecondPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second_page)
+        bindingClass = ActivitySecondPageBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        bindingClass.Option1.setText(R.string.la_persona)
+        bindingClass.Option2.setText(R.string.la_citta)
+        bindingClass.Option3.setText(R.string.bicicletta)
+        bindingClass.Option4.setText(R.string.uomo)
+        bindingClass.NextQuestion2Id.visibility = View.GONE
+        bindingClass.NextQuestion2Id.setText(R.string.next)
+
+
+        bindingClass.Option1.setOnClickListener{
+            this.showIncorrectMessageAnswer()
+        }
+        bindingClass.Option2.setOnClickListener{
+            this.showCorrectMessageAnswer()
+        }
+        bindingClass.Option3.setOnClickListener{
+            this.showIncorrectMessageAnswer()
+        }
+        bindingClass.Option4.setOnClickListener{
+            this.showIncorrectMessageAnswer()
+        }
     }
+    fun onClickGoToThirdActivity(view: View){
+
+    }
+    private fun showIncorrectMessageAnswer(){
+        bindingClass.AnswerCommentId.setText(R.string.incorrect_answer)
+        bindingClass.AnswerCommentId.visibility = View.VISIBLE
+
+    }
+    private fun showCorrectMessageAnswer(){
+        bindingClass.Option1.visibility = View.GONE
+        bindingClass.Option2.visibility = View.GONE
+        bindingClass.Option3.visibility = View.GONE
+        bindingClass.Option4.visibility = View.GONE
+        bindingClass.AnswerCommentId.visibility = View.VISIBLE
+        bindingClass.AnswerCommentId.setText(R.string.correct_answer)
+        bindingClass.NextQuestion2Id.visibility = View.VISIBLE
+    }
+
 }
