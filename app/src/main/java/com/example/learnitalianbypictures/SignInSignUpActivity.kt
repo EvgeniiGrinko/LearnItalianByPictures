27beb03c4b871637a6391f67fea31cf7660a6b93
EvgeniiGrinko <jenek0907@mail.ru>
2023-03-20 11:59:17 +0500

    login page edited

--- app/src/main/java/com/example/learnitalianbypictures/SignInSignUpActivity.kt
+++ app/src/main/java/com/example/learnitalianbypictures/SignInSignUpActivity.kt
@@ -1,29 +1,86 @@
 package com.example.learnitalianbypictures
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.string
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.example.learnitalianbypictures.databinding.ActivitySignInSignUpBinding
+
 
 class SignInSignUpActivity : AppCompatActivity() {
     lateinit var bindingClass: ActivitySignInSignUpBinding
+    lateinit var editName: EditText
+    lateinit var editSurname: EditText
+    lateinit var editLogin: EditText
+    lateinit var editPassword: EditText
+    var code: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivitySignInSignUpBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-        bindingClass.doneButton.setText(R.string.done)
+        bindingClass.doneLoginButton.setText(R.string.done)
         bindingClass.showAvatarImageButton.setText(R.string.avatar)
 
+        code = intent.getIntExtra("sign", 1)
+        if (code == 111) {
+            bindingClass.editTextPersonName.visibility = View.GONE
+            bindingClass.editTextPersonSurname.visibility = View.GONE
+        }
     }
 
-    fun onClickAvatarButton(view: View){
+    fun onClickAvatarButton(view: View) {
         bindingClass.avatarImage.visibility = View.VISIBLE
 
     }
-    fun onClickBack(view: View){
-        intent.putExtra("sign", "ok")
-        setResult(RESULT_OK, intent)
-        finish()
+
+
+
+    fun onClickBack(view: View) {
+        editName = findViewById(R.id.editTextPersonName)
+        editSurname = findViewById(R.id.editTextPersonSurname)
+        editLogin = findViewById(R.id.editTextPersonLogin)
+        editPassword = findViewById(R.id.editTextPersonPassword)
+        val text: String = editLogin.text.toString().trim()
+        val text2: String = editPassword.text.toString().trim()
+        val text3: String = editName.text.toString().trim()
+        val text4: String = editSurname.text.toString().trim()
+        if (code == 111) {
+
+            if (TextUtils.isEmpty(text)) {
+                bindingClass.editTextPersonLogin.error = "Please Enter a Login!"
+            } else if(TextUtils.isEmpty(text2)){
+                bindingClass.editTextPersonPassword.error = "Please Enter a Password!"
+            } else {
+                intent.putExtra(
+                    "sign_in", arrayOf( text, text2)
+                )
+                setResult(111, intent)
+                finish()
+            }
+        } else if (code == 222) {
+
+            if (TextUtils.isEmpty(text3)) {
+                bindingClass.editTextPersonName.error = "Please Enter a Name!"
+            } else if(TextUtils.isEmpty(text4)){
+                bindingClass.editTextPersonSurname.error = "Please Enter a Surname!"
+            } else if(TextUtils.isEmpty(text)){
+                bindingClass.editTextPersonLogin.error = "Please Enter a Login!"
+            } else if(TextUtils.isEmpty(text2)){
+                bindingClass.editTextPersonPassword.error = "Please Enter a Password!"
+            } else {
+                intent.putExtra(
+                    "sign_up", arrayOf( text3, text4, text, text2)
+
+                )
+                setResult(222, intent)
+                finish()
+            }
+
+
+
+        }
     }
 }
--- app/src/main/java/com/example/learnitalianbypictures/StartActivity.kt
+++ app/src/main/java/com/example/learnitalianbypictures/StartActivity.kt
@@ -1,16 +1,18 @@
 package com.example.learnitalianbypictures
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
-import com.example.learnitalianbypictures.databinding.ActivityMainBinding
 import com.example.learnitalianbypictures.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
     lateinit var bindingClass: ActivityStartBinding
+    lateinit var editName: String
+    lateinit var editSurname: String
+    lateinit var editLogin: String
+    lateinit var editPassword: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,18 +29,27 @@ class StartActivity : AppCompatActivity() {
     fun onClickSignInButton(view: View) {
         val intent = Intent(this, SignInSignUpActivity::class.java)
         intent.putExtra("sign", 111)
-
         resultLauncher.launch(intent)
     }
 
 
     var resultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == 111) {
-                val data: Intent? = result.data
+            if (result.resultCode == 222) {
+                val it = result.data?.getStringArrayExtra("sign_up")
+                editName = it?.get(0).toString()
+                editSurname = it?.get(1).toString()
+                editLogin = it?.get(2).toString()
+                editPassword = it?.get(3).toString()
+                bindingClass.welcomingText.text = "Welcome, " + it?.get(2).toString()
 
-            } else if (result.resultCode == 222) {
-                val data: Intent? = result.data
+
+            } else if (result.resultCode == 111) {
+                val it = result.data?.getStringArrayExtra("sign_in")
+                editLogin = it?.get(0).toString()
+                editPassword = it?.get(1).toString()
+                bindingClass.welcomingText.text = "Welcome, " + it?.get(0).toString()
+
 
             }
             bindingClass.signInButton.visibility = View.GONE
